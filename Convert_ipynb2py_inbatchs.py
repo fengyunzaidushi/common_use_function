b14be6aa02f062320dca03f382508096fda67144
fengyunzaidushi <53250064+fengyunzaidushi@users.noreply.github.com>
2023-10-30 21:18:45 +0800

    Update Convert_ipynb2py_inbatchs.py

--- Convert_ipynb2py_inbatchs.py
+++ Convert_ipynb2py_inbatchs.py
@@ -1,10 +1,11 @@
+
 import os
 from nbconvert import PythonExporter
 import json
 import nbformat
 # 要转换的文件夹路径
 root_folder_path = '/mnt/sda/github/10yue/openai-cookbook/ceshi'
-
+import re
 # 创建一个导出器对象
 exporter = PythonExporter()
 
@@ -21,6 +22,15 @@ for foldername, subfolders, filenames in os.walk(root_folder_path):
 
             # 导出为 Python 格式
             python_script, _ = exporter.from_notebook_node(notebook_content)
+            python_script = python_script.replace('get_ipython().system', '#')
+            python_script = python_script.replace('display', '#display')
+            rex = re.compile(r'# In.*')
+            for item in rex.findall(python_script):
+                python_script = python_script.replace(item, '')
+
+            # print(repr(python_script))
+            while '\n\n\n' in python_script:
+                python_script = python_script.replace('\n\n\n', '\n\n')
 
             # 写入到新的 Python 文件
             with open(output_path, 'w', encoding='utf-8') as python_file:
